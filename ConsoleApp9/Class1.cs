4c178efe0556373a36174aa90a8e804f6468d60c
a.bogunov <bogunov_aa@edu.bstu.ru>
2024-03-16 16:40:41 +0300

    fix

--- ConsoleApp9/Class1.cs
+++ ConsoleApp9/Class1.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp9
 {
     internal class Class1
     {
+        public void DoSmth()
+        {
+            Console.WriteLine("Hello world2!!!");
+        }
     }
 }
